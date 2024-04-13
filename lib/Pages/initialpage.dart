2791d7b9b614fea7102f7b88fdf2e14f53ead051
Manoutofpower <ha19034@bristol.ac.uk>
2024-04-13 13:52:46 +0100

    SSSCRRX-23 Initial initial

--- lib/Pages/initialpage.dart
+++ lib/Pages/initialpage.dart
@@ -1,58 +1,68 @@
 import 'package:flutter/material.dart';
-import 'package:dio/dio.dart';
 import 'loginpage.dart';
 import 'registerpage.dart';
 
 class InitialPage extends StatefulWidget {
-  const InitialPage();
+  const InitialPage({Key? key}) : super(key: key);
 
   @override
   State<InitialPage> createState() => _InitialPageState();
 }
 
 class _InitialPageState extends State<InitialPage> {
-  Response<dynamic>? _response1;
-  Response<dynamic>? _response2;
-
-  @override
-  void initState() {
-    super.initState();
-  }
-
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Initial Page'),
-      ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
-            SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => LoginPage()),
-                );
-              },
-              child: Text('Login'),
-            ),
-            SizedBox(height: 10),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => RegisterPage()),
-                );
-              },
-              child: Text('Register'),
-            ),
-          ],
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              SizedBox(height: 20),
+              Text(
+                'Essay Scorer',
+                style: TextStyle(
+                  fontSize: 36,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(height: 10),
+              Text(
+                'Unlock your writing potential',
+                style: TextStyle(
+                  fontSize: 18,
+                ),
+              ),
+              SizedBox(height: 50),
+              Card(
+                elevation: 5,
+                child: Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: <Widget>[
+                      _buildButton(context, 'Log In', LoginPage()),
+                      SizedBox(height: 20),
+                      _buildButton(context, 'Register', RegisterPage()),
+                    ],
+                  ),
+                ),
+              ),
+              SizedBox(height: 20),
+            ],
+          ),
         ),
+      ),
+    );
+  }
+
+  Widget _buildButton(BuildContext context, String text, Widget page) {
+    return SizedBox(
+      width: 200,
+      height: 50,
+      child: ElevatedButton(
+        onPressed: () => Navigator.push(context, MaterialPageRoute(builder: (_) => page)),
+        child: Text(text),
       ),
     );
   }
