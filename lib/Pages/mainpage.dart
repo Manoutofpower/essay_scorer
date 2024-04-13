54c8689604d2c83f43bad58e146da4371875466b
Manoutofpower <ha19034@bristol.ac.uk>
2024-04-13 15:31:39 +0100

    SSSCRRX-14 Basic Main Page

--- lib/Pages/mainpage.dart
+++ lib/Pages/mainpage.dart
@@ -1,75 +1,26 @@
 import 'package:flutter/material.dart';
-import 'userpage.dart';
-import 'taskspage.dart'; // Import the task page
+import 'taskspage.dart';
 
 class MainPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('MainPage'),
-        automaticallyImplyLeading: false, // Remove the back button
-        actions: <Widget>[
-          IconButton(
-            icon: CircleAvatar(
-              backgroundImage:
-
-                  AssetImage('image path'), // Image Path
-
-            ),
+        title: Text('Main Page'),
+        automaticallyImplyLeading: false,
+      ),
+      body: SafeArea(
+        child: Center(
+          child: ElevatedButton(
             onPressed: () {
               Navigator.push(
                 context,
-                MaterialPageRoute(builder: (context) => UserPage()),
+                MaterialPageRoute(builder: (context) => TasksPage()),
               );
             },
-          ),
-        ],
-      ),
-      body: SafeArea(
-        child: Padding(
-          padding: EdgeInsets.all(16.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              SizedBox(height: 20),
-              // Adjust layout
-              _buildHealthCard('Have Passed', '** Tasks', Icons.hotel_outlined),
-              _buildHealthCard(
-                  'Kept Learning', '** Days', Icons.directions_walk_outlined),
-              SizedBox(height: 16),
-              // Add spacing to separate buttons
-              _buildCustomButton('Tasks', Icons.assignment, context),
-              // Add a new custom button
-            ],
+            child: Text('Go to Tasks Page'),
           ),
         ),
-      ),
-    );
-  }
-
-  Widget _buildHealthCard(String title, String data, IconData icon) {
-    return Card(
-      child: ListTile(
-        title: Text(title),
-        subtitle: Text(data),
-        leading: Icon(icon, size: 48), // Adjust icon size for regular display
-      ),
-    );
-  }
-
-  Widget _buildCustomButton(String title, IconData icon, BuildContext context) {
-    return TextButton(
-      onPressed: () {
-        Navigator.push(
-          context,
-          MaterialPageRoute(
-              builder: (context) => TasksPage()), // Navigate to the task page
-        );
-      },
-      child: ListTile(
-        title: Text(title),
-        leading: Icon(icon),
       ),
     );
   }
