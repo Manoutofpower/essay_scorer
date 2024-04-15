9d639b51937c9c0496d9fc500e6dc27fb6370d3f
Manoutofpower <ha19034@bristol.ac.uk>
2024-04-15 04:57:36 +0100

    SSSCRRX-14 SSSCRRX-15 Main Page Design

--- lib/Pages/mainpage.dart
+++ lib/Pages/mainpage.dart
@@ -1,5 +1,6 @@
-import 'package:essay_scorer/global.dart';
 import 'package:flutter/material.dart';
+import 'suggestionpage.dart';
+import '../global.dart';
 import 'userpage.dart';
 import 'taskspage.dart';
 import 'favouritepage.dart';
@@ -10,46 +11,105 @@ class MainPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Main Page'),
+        title: Text('Main Page', style: TextStyle(color: Colors.white)),
+        backgroundColor: Colors.blueGrey,
         automaticallyImplyLeading: false,
         actions: <Widget>[
           IconButton(
             icon: CircleAvatar(
               backgroundImage: AssetImage('assets/fanta.jpg'),
             ),
-            onPressed: () => Navigator.push(context, MaterialPageRoute(builder: (context) => UserPage())),
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => UserPage()),
+              );
+            },
           ),
           IconButton(
-            icon: Icon(Icons.lightbulb_outline),
-            onPressed: () {},
+            icon: Icon(Icons.lightbulb_outline, color: Colors.white),
+            onPressed: latestResult != null
+                ? () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (context) => SuggestionPage(result: latestResult!)),
+              );
+            }
+                : null,
             tooltip: 'Get Suggestions',
           ),
         ],
       ),
-      body: GridView.count(
-        crossAxisCount: 2,
-        children: <Widget>[
-          _buildCard('Have Passed', '** Tasks', Icons.hotel_outlined, context),
-          _buildCard('Kept Learning', '$keptLearningDays Days', Icons.directions_walk_outlined, context),
-          _buildCard('Tasks', '', Icons.assignment, context, onTap: () => Navigator.push(context, MaterialPageRoute(builder: (context) => TasksPage()))),
-          _buildCard('Saved Questions', '${favoriteQuestions.length} questions', Icons.bookmark, context,  onTap: () => Navigator.push(context, MaterialPageRoute(builder: (context) => FavoritesPage()))),
-          _buildCard('Daily Improvement', '', Icons.lightbulb_outline, context, onTap: () => Navigator.push(context, MaterialPageRoute(builder: (context) => QuizPage()))),
-        ],
+      backgroundColor: Colors.grey[850],
+      body: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.all(16.0),
+          child: GridView.count(
+            crossAxisCount: 2,
+            crossAxisSpacing: 15,
+            mainAxisSpacing: 15,
+            childAspectRatio: 1.0,
+            children: <Widget>[
+              _buildCard('Have Passed', '** Tasks', Icons.hotel_outlined,
+                  context, Colors.amber[400]!),
+              _buildCard('Kept Learning', '$keptLearningDays Days',
+                  Icons.directions_walk_outlined, context, Colors.orange[400]!),
+              _buildCard('Tasks', '', Icons.assignment, context,
+                  Colors.amberAccent[700]!, onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => TasksPage()),
+                    );
+                  }),
+              _buildCard(
+                  'Saved Questions',
+                  '${favoriteQuestions.length} questions',
+                  Icons.bookmark,
+                  context,
+                  Colors.redAccent[400]!, onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => FavoritesPage()),
+                );
+              }),
+              _buildCard('Daily Improvement', '', Icons.lightbulb_outline,
+                  context, Colors.lightBlueAccent[400]!, onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => QuizPage()),
+                    );
+                  }),
+            ],
+          ),
+        ),
       ),
     );
   }
 
-  Widget _buildCard(String title, String data, IconData icon, BuildContext context, {VoidCallback? onTap}) {
+  Widget _buildCard(String title, String data, IconData icon,
+      BuildContext context, Color iconColor,
+      {VoidCallback? onTap}) {
     return GestureDetector(
       onTap: onTap,
       child: Card(
+        color: Colors.blueGrey[900],
+        elevation: 5,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(15),
+        ),
         child: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Icon(icon, size: 48),
-              Text(title, style: TextStyle(fontWeight: FontWeight.bold)),
-              Text(data),
+              Icon(icon, size: 48, color: iconColor),
+              SizedBox(height: 10),
+              Text(title,
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold, color: Colors.white70)),
+              if (data.isNotEmpty) SizedBox(height: 5),
+              if (data.isNotEmpty)
+                Text(data, style: TextStyle(color: Colors.white70)),
             ],
           ),
         ),
