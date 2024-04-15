ff86f3a1f9a93ec0d4ad544b0292186b0e1b6d49
Manoutofpower <ha19034@bristol.ac.uk>
2024-04-15 03:41:20 +0100

    SSSCRRX-23 Color Deisgn

--- lib/Pages/initialpage.dart
+++ lib/Pages/initialpage.dart
@@ -13,6 +13,7 @@ class _InitialPageState extends State<InitialPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey[850],
       body: Center(
         child: SingleChildScrollView(
           child: Column(
@@ -22,6 +23,7 @@ class _InitialPageState extends State<InitialPage> {
               Text(
                 'Essay Scorer',
                 style: TextStyle(
+                  color: Colors.grey[300],
                   fontSize: 36,
                   fontWeight: FontWeight.bold,
                 ),
@@ -30,20 +32,22 @@ class _InitialPageState extends State<InitialPage> {
               Text(
                 'Unlock your writing potential',
                 style: TextStyle(
+                  color: Colors.grey[400],
                   fontSize: 18,
                 ),
               ),
               SizedBox(height: 50),
               Card(
+                color: Colors.grey[800],
                 elevation: 5,
                 child: Padding(
                   padding: const EdgeInsets.all(20.0),
                   child: Column(
                     mainAxisSize: MainAxisSize.min,
                     children: <Widget>[
-                      _buildButton(context, 'Log In', LoginPage()),
+                      _buildButton(context, 'Log In', LoginPage(), Colors.deepPurple[300]!),
                       SizedBox(height: 20),
-                      _buildButton(context, 'Register', RegisterPage()),
+                      _buildButton(context, 'Register', RegisterPage(), Colors.teal[300]!),
                     ],
                   ),
                 ),
@@ -56,12 +60,16 @@ class _InitialPageState extends State<InitialPage> {
     );
   }
 
-  Widget _buildButton(BuildContext context, String text, Widget page) {
+  Widget _buildButton(BuildContext context, String text, Widget page, Color color) {
     return SizedBox(
       width: 200,
       height: 50,
       child: ElevatedButton(
         onPressed: () => Navigator.push(context, MaterialPageRoute(builder: (_) => page)),
+        style: ElevatedButton.styleFrom(
+          foregroundColor: Colors.white,
+          backgroundColor: color,
+        ),
         child: Text(text),
       ),
     );
