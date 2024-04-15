7c4955ebebd6c6e58694a6475a11a22706c524c6
Manoutofpower <ha19034@bristol.ac.uk>
2024-04-15 01:41:06 +0100

    SSSCRRX-14 SSSCRRX-18 Upgrade pubspec

---
+++ lib/global.dart
@@ -0,0 +1,9 @@
+// global.dart
+import 'package:essay_scorer/models/question.dart';
+import 'models/result.dart';
+
+List<Question> favoriteQuestions = [];
+
+Result? latestResult;
+
+int keptLearningDays = 0;
